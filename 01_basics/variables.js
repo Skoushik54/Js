9bec9bde3b69e73180ca61a7055981c9d0720658
Skoushik54 <140392717+Skoushik54@users.noreply.github.com>
2025-02-23 18:39:32 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,9 @@
+const accountId = 554
+let accountEmail= "abc@gmail.com"
+var accountPassword="12345"
+accountCity="Hyderabad"
+// accountId=2
+accountPassword="vbfk"
+accountEmail="jsndf"
+console.log(accountId);
+console.table([accountEmail,accountId,accountEmail,accountCity])
